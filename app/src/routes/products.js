c5b055ed01d6226ae8b9723e003ac0eb539af30b
Lisandro Palavecino <119263397+LisandroP57@users.noreply.github.com>
2023-02-14 01:10:17 -0300

    add coments

--- app/src/routes/products.js
+++ app/src/routes/products.js
@@ -9,15 +9,17 @@ const productsController = require("../controllers/productsController");
 router.get('/carrito', productsController.carrito);
 router.get('/vistaProducto', productsController.vistaProducto);
 
-
         /* Detail product
-        router.get('/detail/:id/', productsController.detail); */
+router.get('/detail/:id/', productsController.detail); */
+// Crear detail, store, destroy y update en productsController.
 
         /* Create product */
 router.get('/create', productsController.create);
+/* router.post('/', productsController.store);  */
 
         /* Edit product */
 router.get('/edit/:id', productsController.edit);
+/* router.put('/edit/:id', productsController.update);  */
 
 
         /* Delete product
