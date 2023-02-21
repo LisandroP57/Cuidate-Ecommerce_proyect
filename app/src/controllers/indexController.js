3e070d4e40d0b30adabb8a6a4aa9b17f5a79ed2d
Lisandro Palavecino <119263397+LisandroP57@users.noreply.github.com>
2023-02-21 16:36:10 -0300

    Agregando vistas resultados, modificando css y moviendo lineas codigo de index y products controller

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -8,14 +8,22 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
 	index: (req, res) => {
-
 		let productsInSale = products.filter(product => product.category === "in-sale");
 		let productsVisited = products.filter(product => product.category === "visited");
-
 		res.render("index",{
 			productsVisited,
 			productsInSale,
 			toThousand
         })
     },
+
+	search: (req, res) => {
+		let { keywords } = req.query
+		let results = products.filter(product => product.name.toLowerCase() === keywords.toLowerCase())
+		res.render(`results`, {
+			keywords,
+			results,
+			toThousand,
+		})
+	},
 };
