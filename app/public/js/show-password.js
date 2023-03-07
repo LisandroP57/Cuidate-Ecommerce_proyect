cc108cec49617ad3ced62c54b25b6e36891f5d91
Lisandro Palavecino <119263397+LisandroP57@users.noreply.github.com>
2023-03-06 23:11:50 -0300

    Agregando funcion de ver contraseña/eliminando span de validaciones asi posteriormente ponemos if/editando rutas /home a /

---
+++ app/public/js/show-password.js
@@ -0,0 +1,8 @@
+function showPassword() {
+    var x = document.getElementById("passwordInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
