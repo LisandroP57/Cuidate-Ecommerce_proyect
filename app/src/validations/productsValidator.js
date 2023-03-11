4518ce63d5241c22f7204a3b2011c2c251113226
Lisandro Palavecino <119263397+LisandroP57@users.noreply.github.com>
2023-03-11 14:54:40 -0300

    editando visual header de menu online

--- app/src/validations/productsValidator.js
+++ app/src/validations/productsValidator.js
@@ -1,19 +1,22 @@
 const { check } = require("express-validator");
-const path = require("path");
+
 module.exports = [
     check('name')
     .notEmpty()
-    .withMessage("Debe contener el nombre del producto").bail().isLength({ min: 5, max: 15 })
+    .withMessage("Debe contener el nombre del producto").bail()
+    .isLength({ min: 5, max: 15 })
     .withMessage("El nombre tiene que tener entre 5 y 15 caracteres"),
     
     check('description')
     .notEmpty()
-    .withMessage("Debe contener su descripción").bail().isLength({ min: 15 })
+    .withMessage("Debe contener su descripción").bail()
+    .isLength({ min: 15 })
     .withMessage("La descripcion tiene que tener un mínimo de 15 caracteres"),
     
     check('price')
     .notEmpty()
-    .withMessage("Debe contener el precio").isFloat({ min: 1 })
+    .withMessage("Debe contener el precio")
+    .isFloat({ min: 1 })
     .withMessage("El producto lo estarías dando gratis..."),
     
     check('discount')
