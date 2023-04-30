01fe78de964f9cbc84555bd4551403262ab07d6b
LisandroP57 <lisandropalavecino1@gmail.com>
2023-04-30 13:51:13 -0300

    Implementando sequelize de carrito y items de carrito/Modificando y creandolo en la DB (copiar el script y ejecutar o va a romper)/modificando css

---
+++ app/src/database/models/Cart.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Cart";
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(10),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        userId: {
+            type: dataTypes.INTEGER(10),
+            allowNull: false,
+        },
+        state: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+    }
+
+    let config = {
+        tableName: "carts",
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    };
+
+    const CART = sequelize.define(alias, cols, config);
+
+    CART.associate = (models) => {
+
+        CART.belongsTo(models.User, {
+            as: "user",
+            foreignKey: "userId",
+        })
+
+        CART.hasMany(models.cartItem, {
+            as: "cartItems",
+            foreignKey: "cartId",
+        });
+    }
+
+    return CART;
+}
