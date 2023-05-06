ea14cf3d4fa52aaebac66ae571ef725b8d1bbbb7
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-05-05 22:22:13 -0300

    agregue una nueva expresion regular regExAlpha para alfanumericos lo uso en los campos input cuando el usuario ingresa texto

--- app/public/js/validations/login.js
+++ app/public/js/validations/login.js
@@ -11,6 +11,7 @@ window.addEventListener("load", () => {
         $passErrors = qs('#passErrors'),
         $terms = qs('#flexCheckDefault'),
         $termsErrors = qs('#termsErrors'),
+        regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
         regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
         regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
 
