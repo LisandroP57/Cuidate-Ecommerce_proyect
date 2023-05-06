01c34a2285d79a386f4a055057ea13cdc8f4e9c4
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-05-05 22:22:54 -0300

    la muevo para registro, porque es para caracteres alfanumericos

--- app/public/js/validations/login.js
+++ app/public/js/validations/login.js
@@ -11,7 +11,6 @@ window.addEventListener("load", () => {
         $passErrors = qs('#passErrors'),
         $terms = qs('#flexCheckDefault'),
         $termsErrors = qs('#termsErrors'),
-        regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
         regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
         regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
 
