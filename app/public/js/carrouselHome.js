04e6ee6913afddb8261eba968c3e26f6380b285b
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-05-09 17:00:19 -0300

    acomode la funcion para que me corra las imagenes automaticamente

--- app/public/js/carrouselHome.js
+++ app/public/js/carrouselHome.js
@@ -1,101 +1,52 @@
-$(function() {
-  "use strict";
-  var body = $("body"),
-      active = $(".slider ol li, .slider .controll"),
-      controll = $(".slider .controll"),
-      playpause = $(".playpause"),
-      sliderTime = 1,
-      sliderWait = 3000,
-      i = 999,
-      autoRun,
-      stop = false;
-  // Reset
-  $(".slider ul li:first").css("left", 0);
-  // Run Slider
-  function runSlider(what) {
-      what.addClass("active").siblings("li, span").removeClass("active");
-  }
-  // slider gsap
-  function gsapSlider(whose, left) {
-      i++;
-      if (whose.hasClass("active")) {
-          TweenMax.fromTo(
-              ".slider ul li.active",
-              sliderTime,
-              { zIndex: i, left: left },
-              { left: 0 }
-          );
-      }
-  }
-  // Active
-  active.on("click", function() {
-      runSlider($(this));
-  });
-  // Arrow left
-  controll.first().on("click", function() {
-      var slide = $(".slider ul li.active, .slider ol li.active").is(
-          ":first-of-type"
-      )
-          ? $(".slider ul li:last, .slider ol li:last")
-          : $(".slider ul li.active, .slider ol li.active").prev("li");
-      runSlider(slide);
-      gsapSlider(slide, "100%");
-  });
-  // Arrow right
-  controll.last().on("click", function() {
-      var slide = $(".slider ul li.active, .slider ol li.active").is(
-          ":last-of-type"
-      )
-          ? $(".slider ul li:first, .slider ol li:first")
-          : $(".slider ul li.active, .slider ol li.active").next("li");
-      runSlider(slide);
-      gsapSlider(slide, "-100%");
-  });
-  // Point
-  $(".slider ol li").on("click", function() {
-      var start = $(".slider ul li.active").index();
-      var slide = $(".slider ul li").eq($(this).index());
-      runSlider(slide);
-      var end = $(".slider ul li.active").index();
-      if (start > end) {
-          gsapSlider(slide, "100%");
-      }
-      if (start < end) {
-          gsapSlider(slide, "-100%");
-      }
-  });
-  // Auto run slider
-  function autoRunSlider() {
-      if (body.css("direction") === "ltr" && stop === false) {
-          autoRun = setInterval(function() {
-              controll.last().click();
-          }, sliderWait);
-      } else if (body.css("direction") === "rtl" && stop === false) {
-          autoRun = setInterval(function() {
-              controll.first().click();
-          }, sliderWait);
-      }
-  }
-  autoRunSlider();
-  // When hover
-  active.on("mouseenter", function() {
-      if (stop === false) {clearInterval(autoRun);}
-  });
-  active.on("mouseleave", function() {
-      if (stop === false) {autoRunSlider();}
-  });
-  // play pause click
-  playpause.on("click", function() {
-      $(this).toggleClass("fa-play-circle-o fa-pause-circle-o");
-      if (playpause.hasClass("fa-play-circle-o")) {
-          stop = true;
-          clearInterval(autoRun);
-          $(this).attr('title', 'play');
-      }
-      if (playpause.hasClass("fa-pause-circle-o")) {
-          stop = false;
-          autoRunSlider();
-          $(this).attr('title', 'pause');
-      }
-  });
-});
+let slideIndex = 1; //Establece el slide inicial
+showSlides(slideIndex)
+
+// Controles de anterior y siguiente
+function plusSlides(number) {
+    showSlides(slideIndex += number)
+}
+
+// Controles de imagen en miniatura
+function currentSlide (number) {
+    showSlides(slideIndex = number)
+}
+
+function showSlides (number) {
+    let i;
+    let slides = document.getElementsByClassName("slides"); //captura todos los elementos del DOM que tengan la clase "slides"
+    let dots = document.getElementsByClassName("dot");//captura todos los elementos del DOM que tengan la clase "dot"
+    /* Si el numero recibido por parámetro es mayor a
+     la cantidad de elementos que contienen la clase slides, 
+     setea la variable slides a 1. Por lo tanto la imagen que se muestra 
+     va a ser la primera, con esto evitamos que haya un error al pasar un
+     índice que no corresponda a la longitud del array */
+    if (number > slides.length) {
+        slideIndex = 1
+    }
+    /* si el número recibido por parámetro es menor a 1, setea la variable
+    slideIndex a el largo del array, con lo cual evitamos el error por
+    un indice menor. Muestra la ultima imagen. */
+    if (number < 1 ) {
+        slideIndex = slides.length
+    }
+    /* Recorre el array de slides y en cada iteración coloca display none a cada elemento (nodo) del array */
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    /* Recorre el array de puntos y en cada iteración reemplaza la clase 
+    "dot-active" de todos los elementos que la contengan */
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" dot-active", "");
+    }
+
+    /* Según el índice indicado en la variable, se le dará display "block" al slide que corresponda */
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " dot-active";
+}
+
+/* EXTRAAAA */
+
+setInterval(() => {
+    currentSlide(slideIndex + 1)
+}, 5000);  
+
