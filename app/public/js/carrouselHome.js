5deefd60d6944ba11efc0ed800139ae83b1b3923
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-05-09 17:04:20 -0300

    volvi a version del comienzo para probar las medidas y que no rompa

--- app/public/js/carrouselHome.js
+++ app/public/js/carrouselHome.js
@@ -1,49 +1,104 @@
-let slideIndex = 1; //Establece el slide inicial
-showSlides(slideIndex)
-
-// Controles de anterior y siguiente
-function plusSlides(number) {
-    showSlides(slideIndex += number1)
+document.getElementById("outer3").addEventListener("click", toggleState3);
+    
+function toggleState3() {
+  let galleryView = document.getElementById("galleryView")
+  let tilesView = document.getElementById("tilesView")
+  let outer = document.getElementById("outer3");
+  let slider = document.getElementById("slider3");
+  let tilesContainer = document.getElementById("tilesContainer");
+  if (slider.classList.contains("active")) {
+    slider.classList.remove("active");
+    outer.classList.remove("outerActive");
+    galleryView.style.display = "flex";
+    tilesView.style.display = "none";
+    
+    while (tilesContainer.hasChildNodes()) {
+      tilesContainer.removeChild(tilesContainer.firstChild)
+      }  
+  } else {
+    slider.classList.add("active");
+    outer.classList.add("outerActive");
+    galleryView.style.display = "none";
+    tilesView.style.display = "flex";
+     
+    for (let i = 0; i < imgObject.length - 1; i++) {
+      let tileItem = document.createElement("div");
+      tileItem.classList.add("tileItem");
+      tileItem.style.background =  "url(" + imgObject[i] + ")";
+      tileItem.style.backgroundSize = "contain";  
+      tilesContainer.appendChild(tileItem);      
+    }
+  };
 }
 
+let imgObject = [
+  "images/banners/1.png",
+  "images/banners/2.png",
+  "images/banners/3.png",
+  "images/banners/4.png",
+  "images/banners/5.png",
+  "images/banners/6.png",
+  "images/banners/7.png",
+  "images/banners/8.png",
+];
+
+let mainImg = 0;
+let prevImg = imgObject.length - 1;
+let nextImg = 1;
+
+function loadGallery() {
+
+  let mainView = document.getElementById("mainView");
+  mainView.style.background = "url(" + imgObject[mainImg] + ")";
+
+  let leftView = document.getElementById("leftView");
+  leftView.style.background = "url(" + imgObject[prevImg] + ")";
+  
+  let rightView = document.getElementById("rightView");
+  rightView.style.background = "url(" + imgObject[nextImg] + ")";
+  
+  let linkTag = document.getElementById("linkTag")
+  linkTag.href = imgObject[mainImg];
+
+};
+
+function scrollRight() {
+  
+  prevImg = mainImg;
+  mainImg = nextImg;
+  if (nextImg >= (imgObject.length -1)) {
+    nextImg = 0;
+  } else {
+    nextImg++;
+  }; 
+  loadGallery();
+};
+
+function scrollLeft() {
+  nextImg = mainImg
+  mainImg = prevImg;
+   
+  if (prevImg === 0) {
+    prevImg = imgObject.length - 1;
+  } else {
+    prevImg--;
+  };
+  loadGallery();
+};
+
+document.getElementById("navRight").addEventListener("click", scrollRight);
+document.getElementById("navLeft").addEventListener("click", scrollLeft);
+document.getElementById("rightView").addEventListener("click", scrollRight);
+document.getElementById("leftView").addEventListener("click", scrollLeft);
+document.addEventListener('keyup',function(e){
+    if (e.keyCode === 37) {
+    scrollLeft();
+  } else if(e.keyCode === 39) {
+    scrollRight();
+  }
+});
+
+loadGallery();
 
 
-function showSlides (number) {
-    let i;
-    let slides = document.getElementsByClassName("slides"); //captura todos los elementos del DOM que tengan la clase "slides"
-    let dots = document.getElementsByClassName("dot");//captura todos los elementos del DOM que tengan la clase "dot"
-    /* Si el numero recibido por parámetro es mayor a
-     la cantidad de elementos que contienen la clase slides, 
-     setea la variable slides a 1. Por lo tanto la imagen que se muestra 
-     va a ser la primera, con esto evitamos que haya un error al pasar un
-     índice que no corresponda a la longitud del array */
-    if (number > slides.length) {
-        slideIndex = 1
-    }
-    /* si el número recibido por parámetro es menor a 1, setea la variable
-    slideIndex a el largo del array, con lo cual evitamos el error por
-    un indice menor. Muestra la ultima imagen. */
-    if (number < 1 ) {
-        slideIndex = slides.length
-    }
-    /* Recorre el array de slides y en cada iteración coloca display none a cada elemento (nodo) del array */
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    /* Recorre el array de puntos y en cada iteración reemplaza la clase 
-    "dot-active" de todos los elementos que la contengan */
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" dot-active", "");
-    }
-
-    /* Según el índice indicado en la variable, se le dará display "block" al slide que corresponda */
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " dot-active";
-}
-
-/* EXTRAAAA */
-
-setInterval(() => {
-    currentSlide(slideIndex + 1)
-}, 5000);  
 
