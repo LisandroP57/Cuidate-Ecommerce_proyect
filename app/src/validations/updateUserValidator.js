ce0fbd5ff54e22c4decc121dcdc18c18f2553ee7
RicardoVallejos92 <richard_22_100@hotmail.com>
2023-05-08 23:48:57 -0300

    corrigiendo validaciones back end caracteres de nombre y apellido

--- app/src/validations/updateUserValidator.js
+++ app/src/validations/updateUserValidator.js
@@ -3,11 +3,27 @@ const { check } = require("express-validator");
 module.exports = [
     check("name")
     .notEmpty()
-    .withMessage("El nombre es obligatorio"),
+    .withMessage("El nombre es obligatorio")
+    .custom((value) => {
+        if (!/^[a-zA-Z]{2,}$/.test(value)) {
+          throw new Error(
+            "Ingrese un nombre válido (mínimo 2 caracteres y sin números ni caracteres especiales)"
+          );
+        }
+        return true;
+      }),
 
     check("last_name")
     .notEmpty()
-    .withMessage("El apellido es obligatorio"),
+    .withMessage("El apellido es obligatorio")
+    .custom((value) => {
+        if (!/^[a-zA-Z]{2,}$/.test(value)) {
+          throw new Error(
+            "Ingrese un apellido válido (mínimo 2 caracteres y sin números ni caracteres especiales)"
+          );
+        }
+        return true;
+      }),
 
     check("email")
     .notEmpty()
@@ -15,7 +31,4 @@ module.exports = [
     .isEmail()
     .withMessage("Email inválido"),
 
-    /* check("postal_code")
-    .isNumeric()
-    .withMessage("Solo puedes ingresar números"), */
 ]
