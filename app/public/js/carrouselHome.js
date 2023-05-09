f814f4a94ed06a427e2c7ced4345cb0ffc7a25f8
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-05-09 16:57:49 -0300

    creeando nuevo carrousel

--- app/public/js/carrouselHome.js
+++ app/public/js/carrouselHome.js
@@ -1,104 +1,23 @@
-document.getElementById("outer3").addEventListener("click", toggleState3);
-    
-function toggleState3() {
-  let galleryView = document.getElementById("galleryView")
-  let tilesView = document.getElementById("tilesView")
-  let outer = document.getElementById("outer3");
-  let slider = document.getElementById("slider3");
-  let tilesContainer = document.getElementById("tilesContainer");
-  if (slider.classList.contains("active")) {
-    slider.classList.remove("active");
-    outer.classList.remove("outerActive");
-    galleryView.style.display = "flex";
-    tilesView.style.display = "none";
-    
-    while (tilesContainer.hasChildNodes()) {
-      tilesContainer.removeChild(tilesContainer.firstChild)
-      }  
-  } else {
-    slider.classList.add("active");
-    outer.classList.add("outerActive");
-    galleryView.style.display = "none";
-    tilesView.style.display = "flex";
-     
-    for (let i = 0; i < imgObject.length - 1; i++) {
-      let tileItem = document.createElement("div");
-      tileItem.classList.add("tileItem");
-      tileItem.style.background =  "url(" + imgObject[i] + ")";
-      tileItem.style.backgroundSize = "contain";  
-      tilesContainer.appendChild(tileItem);      
-    }
-  };
-}
+document.addEventListener('DOMContentLoaded', function() {
+  var parent = document.querySelector('.splitview'),
+      topPanel = parent.querySelector('.top'),
+      handle = parent.querySelector('.handle'),
+      skewHack = 0,
+      delta = 0;
 
-let imgObject = [
-  "images/banners/1.png",
-  "images/banners/2.png",
-  "images/banners/3.png",
-  "images/banners/4.png",
-  "images/banners/5.png",
-  "images/banners/6.png",
-  "images/banners/7.png",
-  "images/banners/8.png",
-];
-
-let mainImg = 0;
-let prevImg = imgObject.length - 1;
-let nextImg = 1;
-
-function loadGallery() {
-
-  let mainView = document.getElementById("mainView");
-  mainView.style.background = "url(" + imgObject[mainImg] + ")";
-
-  let leftView = document.getElementById("leftView");
-  leftView.style.background = "url(" + imgObject[prevImg] + ")";
-  
-  let rightView = document.getElementById("rightView");
-  rightView.style.background = "url(" + imgObject[nextImg] + ")";
-  
-  let linkTag = document.getElementById("linkTag")
-  linkTag.href = imgObject[mainImg];
-
-};
-
-function scrollRight() {
-  
-  prevImg = mainImg;
-  mainImg = nextImg;
-  if (nextImg >= (imgObject.length -1)) {
-    nextImg = 0;
-  } else {
-    nextImg++;
-  }; 
-  loadGallery();
-};
-
-function scrollLeft() {
-  nextImg = mainImg
-  mainImg = prevImg;
-   
-  if (prevImg === 0) {
-    prevImg = imgObject.length - 1;
-  } else {
-    prevImg--;
-  };
-  loadGallery();
-};
-
-document.getElementById("navRight").addEventListener("click", scrollRight);
-document.getElementById("navLeft").addEventListener("click", scrollLeft);
-document.getElementById("rightView").addEventListener("click", scrollRight);
-document.getElementById("leftView").addEventListener("click", scrollLeft);
-document.addEventListener('keyup',function(e){
-    if (e.keyCode === 37) {
-    scrollLeft();
-  } else if(e.keyCode === 39) {
-    scrollRight();
+  // If the parent has .skewed class, set the skewHack var.
+  if (parent.className.indexOf('skewed') != -1) {
+      skewHack = 1000;
   }
+
+  parent.addEventListener('mousemove', function(event) {
+      // Get the delta between the mouse position and center point.
+      delta = (event.clientX - window.innerWidth / 2) * 0.5;
+
+      // Move the handle.
+      handle.style.left = event.clientX + delta + 'px';
+
+      // Adjust the top panel width.
+      topPanel.style.width = event.clientX + skewHack + delta + 'px';
+  });
 });
-
-loadGallery();
-
-
-
