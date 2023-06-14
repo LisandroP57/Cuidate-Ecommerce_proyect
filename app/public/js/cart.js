78b267c585b20ea6ad4e0854d5993351abd6694a
LisandroP57 <lisandropalavecino1@gmail.com>
2023-06-14 01:05:52 -0300

    Editando tamaño de header/agrandando iconos-fuentes-font-weight/modificando menu de productos/agregando mas visibilidad a menu hamburguesa/editando vista desfazada detail/modificando edicion de perfil

--- app/public/js/cart.js
+++ app/public/js/cart.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "http://localhost:3001/api";
 const token = localStorage.getItem("token");
 
 const getData = async (url, token) => {
